2277cf61b7eb38af3951782440b0135db596e891
MaahesCr <psm3017@mail.ru>
2022-05-18 00:02:15 +0500

    interim result 17.05

--- scripts/JS/main.js
+++ scripts/JS/main.js
@@ -1,6 +1,37 @@
 let servicesLink = document.querySelector(".header__services-link");
-let expandNavigation = document.querySelector(".expand-navigation");
+let expandNavigation = $('.expand-navigation'); //document.querySelector(".expandNavigation");
 let openNavLinksBool = false;
+
+let headerBurger = $('.header__burger')[0];
+let signingServiceBtn = $('.header__signing-service-btn')[0];
+let burgerContent = $('.header__burger-content-zone');
+let openHeaderBurger = false;
+
+headerBurger.onclick = function () {
+    // смена значка
+    openHeaderBurger = !openHeaderBurger;
+    (openHeaderBurger) ? headerBurger.src = 'UI Kit/Icons/BurgerClouse.svg' : headerBurger.src = 'UI Kit/Icons/Burger.svg'
+    
+    // смена классов для кнопки запись на сервис 
+    signingServiceBtn.classList.toggle('header__signing-service-btn-active');
+
+    // смена класса для содержимого burger-content
+    console.log(burgerContent[0].classList)
+    if (burgerContent.hasClass('header__burger-content-zone-false')) {  
+        console.log('+') 
+        burgerContent.removeClass('header__burger-content-zone-false');  
+        setTimeout(function () {  
+            burgerContent.removeClass('header__burger-content-zone-visuallyhidden');  
+        }, 0.3);  
+    } else {  
+        console.log('-') 
+        burgerContent.addClass('header__burger-content-zone-visuallyhidden'); 
+        burgerContent.one('transitionend', function(e) {  
+            burgerContent.addClass('header__burger-content-zone-false');  
+    });  
+    }
+
+}
 
 servicesLink.onclick = function () {
     // смена значка
@@ -8,7 +39,7 @@ servicesLink.onclick = function () {
     (openNavLinksBool) ? servicesLink.children[1].src = 'UI Kit/Icons/DropdawnOpend.svg' : servicesLink.children[1].src = 'UI Kit/Icons/Dropdawn.svg'
 
     // анимация услуг
-    let expandNavigation = $('.expand-navigation');     //expand-navigation-false hidden
+    //let expandNavigation = $('.expand-navigation');     //expand-navigation-false hidden
     if (expandNavigation.hasClass('expand-navigation-false')) {          //expand-navigation-visuallyhidden visuallyhidden
         expandNavigation.removeClass('expand-navigation-false');  
         setTimeout(function () {  
