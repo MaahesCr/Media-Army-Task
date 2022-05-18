4ae432c1901e549f26335a04b4498dbb2a84e659
MaahesCr <psm3017@mail.ru>
2022-05-18 23:09:22 +0500

    interim result 18.05

--- scripts/JS/main.js
+++ scripts/JS/main.js
@@ -1,7 +1,3 @@
-let servicesLink = document.querySelector(".header__services-link");
-let expandNavigation = $('.expand-navigation'); //document.querySelector(".expandNavigation");
-let openNavLinksBool = false;
-
 let headerBurger = $('.header__burger')[0];
 let signingServiceBtn = $('.header__signing-service-btn')[0];
 let burgerContent = $('.header__burger-content-zone');
@@ -12,27 +8,38 @@ headerBurger.onclick = function () {
     openHeaderBurger = !openHeaderBurger;
     (openHeaderBurger) ? headerBurger.src = 'UI Kit/Icons/BurgerClouse.svg' : headerBurger.src = 'UI Kit/Icons/Burger.svg'
     
-    // смена классов для кнопки запись на сервис 
-    signingServiceBtn.classList.toggle('header__signing-service-btn-active');
+    // смена классов для кнопки запись на сервис (старая реализация)
+    //signingServiceBtn.classList.toggle('header__signing-service-btn-active');
 
+    // смена свойств кнопки
+    if (openHeaderBurger){
+        $('.header__signing-service-btn').animate({padding: '50px 72.5px', borderRadius: 0, marginTop: '47px' ,marginRight: '-130px'}, 300)
+        $('.header__burger').animate({marginTop: '-47px'}, 300)
+    } else {
+        $('.header__signing-service-btn').animate({padding: '12px 20px',  borderRadius: '7px', marginTop: '0px', marginRight: 0 }, 300);
+        $('.header__burger').animate({marginTop: '0px'}, 300)
+    }
+
+    
     // смена класса для содержимого burger-content
-    console.log(burgerContent[0].classList)
     if (burgerContent.hasClass('header__burger-content-zone-false')) {  
-        console.log('+') 
         burgerContent.removeClass('header__burger-content-zone-false');  
         setTimeout(function () {  
             burgerContent.removeClass('header__burger-content-zone-visuallyhidden');  
         }, 0.3);  
     } else {  
-        console.log('-') 
         burgerContent.addClass('header__burger-content-zone-visuallyhidden'); 
         burgerContent.one('transitionend', function(e) {  
             burgerContent.addClass('header__burger-content-zone-false');  
     });  
     }
-
 }
 
+let servicesLink = document.querySelector(".header__services-link");
+let expandNavigation = $('.expand-navigation'); //document.querySelector(".expandNavigation");
+let openNavLinksBool = false;
+
+// кнопка "услуги" 
 servicesLink.onclick = function () {
     // смена значка
     openNavLinksBool = !openNavLinksBool;
@@ -46,10 +53,51 @@ servicesLink.onclick = function () {
             expandNavigation.removeClass('expand-navigation-visuallyhidden');  
         }, 0.3);  
     } else {  
-        expandNavigation.addClass('expand-navigation-visuallyhidden');  
+        expandNavigation.addClass('expand-navigation-visuallyhidden');   
         expandNavigation.one('transitionend', function(e) {  
             expandNavigation.addClass('expand-navigation-false');  
     });  
     }
     
 } 
+
+let arrOfSlides = [$('#first-carusel-img'), $('#second-carusel-img'), $('#third-carusel-img')];
+let currentSlide = 0; 
+let caruselArrowLeft = $("#carusel-arrow-left")[0];
+let caruselArrowRight = $('#carusel-arrow-right')[0];
+
+caruselArrowLeft.onclick = function () {
+    currentSlide = currentSlide % 3;
+
+    arrOfSlides[currentSlide].animate({marginLeft: '-1000px', opacity: 0}, 600)
+
+    setTimeout(function () {  
+        arrOfSlides[currentSlide].removeClass('slider-img-active')
+        currentSlide = currentSlide + 1;
+    }, 300);
+    
+    arrOfSlides[(currentSlide+1)%3].addClass('slider-img-active')     
+    arrOfSlides[(currentSlide+1)%3].animate({marginLeft: '1000px', opacity: 0}, 0) //marginTop: `-${$('.main-section__slider').height()}`}, 0)
+    
+    arrOfSlides[(currentSlide+1)%3].animate({marginLeft: '0', opacity: '100' }, 300)   
+}
+
+caruselArrowRight.onclick = function () {
+    const reversSlides = [0, 1, 2]
+    currentSlide = currentSlide % 3;
+
+    //arrOfSlides[currentSlide].animate({marginLeft: '-1000px', opacity: 0}, 600)
+
+    setTimeout(function () {  
+        //arrOfSlides[currentSlide].removeClass('slider-img-active')
+        //currentSlide = (currentSlide - 1)%3;    //(-0) = 0 (-1) = 2 (-2) = 1 (-3) = 0 
+        //let prevVal = 2;
+        currentSlide = Math.abs((currentSlide - 1)%3);  
+        console.log(currentSlide)
+    }, 300);
+    
+    //arrOfSlides[(currentSlide+1)%3].addClass('slider-img-active')     
+    //arrOfSlides[(currentSlide+1)%3].animate({marginLeft: '1000px', opacity: 0}, 0) //marginTop: `-${$('.main-section__slider').height()}`}, 0)
+    
+    //arrOfSlides[(currentSlide+1)%3].animate({marginLeft: '0', opacity: '100' }, 300)   
+}
